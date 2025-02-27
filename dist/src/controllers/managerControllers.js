c30ea11cfee51e11a10a160b9cd5a5eb4c8afec8
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2025-02-28 00:46:54 +0700

    base code

---
+++ dist/src/controllers/managerControllers.js
@@ -0,0 +1,105 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getManagerProperties = exports.updateManager = exports.createManager = exports.getManager = void 0;
+const client_1 = require("@prisma/client");
+const wkt_1 = require("@terraformer/wkt");
+const prisma = new client_1.PrismaClient();
+const getManager = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { cognitoId } = req.params;
+        const manager = yield prisma.manager.findUnique({
+            where: { cognitoId }
+        });
+        if (manager) {
+            res.json(manager);
+        }
+        else {
+            res.status(404).json({ message: 'Manager not found' });
+        }
+    }
+    catch (error) {
+        res
+            .status(500)
+            .json({ message: `Error retrieving manager: ${error.message}` });
+    }
+});
+exports.getManager = getManager;
+const createManager = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { cognitoId, name, email, phoneNumber } = req.body;
+        const manager = yield prisma.manager.create({
+            data: {
+                cognitoId,
+                name,
+                email,
+                phoneNumber
+            }
+        });
+        res.status(201).json(manager);
+    }
+    catch (error) {
+        res
+            .status(500)
+            .json({ message: `Error creating manager: ${error.message}` });
+    }
+});
+exports.createManager = createManager;
+const updateManager = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { cognitoId } = req.params;
+        const { name, email, phoneNumber } = req.body;
+        const updateManager = yield prisma.manager.update({
+            where: { cognitoId },
+            data: {
+                name,
+                email,
+                phoneNumber
+            }
+        });
+        res.json(updateManager);
+    }
+    catch (error) {
+        res
+            .status(500)
+            .json({ message: `Error updating manager: ${error.message}` });
+    }
+});
+exports.updateManager = updateManager;
+const getManagerProperties = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { cognitoId } = req.params;
+        const properties = yield prisma.property.findMany({
+            where: { managerCognitoId: cognitoId },
+            include: {
+                location: true
+            }
+        });
+        const propertiesWithFormattedLocation = yield Promise.all(properties.map((property) => __awaiter(void 0, void 0, void 0, function* () {
+            var _a;
+            const coordinates = yield prisma.$queryRaw `SELECT ST_asText(coordinates) as coordinates from "Location" where id = ${property.location.id}`;
+            const geoJSON = (0, wkt_1.wktToGeoJSON)(((_a = coordinates[0]) === null || _a === void 0 ? void 0 : _a.coordinates) || '');
+            const longitude = geoJSON.coordinates[0];
+            const latitude = geoJSON.coordinates[1];
+            return Object.assign(Object.assign({}, property), { location: Object.assign(Object.assign({}, property.location), { coordinates: {
+                        longitude,
+                        latitude
+                    } }) });
+        })));
+        res.json(propertiesWithFormattedLocation);
+    }
+    catch (err) {
+        res
+            .status(500)
+            .json({ message: `Error retrieving manager properties: ${err.message}` });
+    }
+});
+exports.getManagerProperties = getManagerProperties;
---
+++ dist/src/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
+const authMiddleware_1 = require("./middleware/authMiddleware");
+/* ROUTE IMPORT */
+const tenantRoutes_1 = __importDefault(require("./routes/tenantRoutes"));
+const managerRoutes_1 = __importDefault(require("./routes/managerRoutes"));
+const propertyRoutes_1 = __importDefault(require("./routes/propertyRoutes"));
+const leaseRoutes_1 = __importDefault(require("./routes/leaseRoutes"));
+const applicationRoutes_1 = __importDefault(require("./routes/applicationRoutes"));
+/* CONFIGURATIONS */
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, helmet_1.default)());
+app.use(helmet_1.default.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use((0, morgan_1.default)('common'));
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use((0, cors_1.default)());
+/* ROUTES */
+app.get('/', (req, res) => {
+    res.send('This is home route');
+});
+app.use('/applications', applicationRoutes_1.default);
+app.use('/properties', propertyRoutes_1.default);
+app.use('/leases', leaseRoutes_1.default);
+app.use('/tenants', (0, authMiddleware_1.authMiddleware)(['tenant']), tenantRoutes_1.default);
+app.use('/managers', (0, authMiddleware_1.authMiddleware)(['manager']), managerRoutes_1.default);
+/* SERVER */
+const port = Number(process.env.PORT) || 3002;
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
+});
